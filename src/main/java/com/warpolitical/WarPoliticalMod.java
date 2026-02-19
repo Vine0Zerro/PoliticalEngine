194b88859e5566dc6f6ac079606bd364f201f148
Vine0Zerro <vine0zerro@gmail.com>
2026-02-19 15:10:26 +0300

    Refactor WarPoliticalMod initialization and integration

--- src/main/java/com/warpolitical/WarPoliticalMod.java
+++ src/main/java/com/warpolitical/WarPoliticalMod.java
@@ -1,13 +1,20 @@
 package com.warpolitical;
 
+import com.warpolitical.bluemap.BlueMapIntegration;
+import com.warpolitical.claim.ClaimEngine;
 import com.warpolitical.commands.CommandRegistrar;
 import com.warpolitical.data.DataManager;
 import com.warpolitical.listeners.ProtectionHandler;
-import com.warpolitical.bluemap.BlueMapIntegration;
+import com.warpolitical.scenario.ScenarioParser;
 import com.warpolitical.world.WorldManager;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.server.MinecraftServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.nio.file.Path;
 
 public class WarPoliticalMod implements ModInitializer {
 
@@ -15,35 +22,89 @@ public class WarPoliticalMod implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     private static WarPoliticalMod instance;
+    private MinecraftServer server;
+
     private DataManager dataManager;
+    private ScenarioParser scenarioParser;
     private WorldManager worldManager;
+    private ClaimEngine claimEngine;
+    private BlueMapIntegration blueMapIntegration;
 
     @Override
     public void onInitialize() {
         instance = this;
         LOGGER.info("WarPoliticalEngine загружается...");
 
-        dataManager = new DataManager();
+        Path configDir = FabricLoader.getInstance()
+                .getConfigDir().resolve(MOD_ID);
+        configDir.toFile().mkdirs();
+
+        this.dataManager = new DataManager(configDir);
+        this.claimEngine = new ClaimEngine(this);
+        this.blueMapIntegration = new BlueMapIntegration(this);
+
+        CommandRegistrar.register(this);
+        ProtectionHandler.register(this);
+
+        ServerLifecycleEvents.SERVER_STARTED.register(this::onServerStarted);
+        ServerLifecycleEvents.SERVER_STOPPING.register(this::onServerStopping);
+
+        LOGGER.info("WarPoliticalEngine зарегистрирован!");
+    }
+
+    private void onServerStarted(MinecraftServer server) {
+        this.server = server;
+        this.worldManager = new WorldManager(this, server);
+        this.scenarioParser = new ScenarioParser(this, server);
+
         dataManager.loadAll();
 
-        worldManager = new WorldManager();
+        if (FabricLoader.getInstance().isModLoaded("bluemap")) {
+            blueMapIntegration.initialize();
+            LOGGER.info("BlueMap обнаружен, интеграция активна");
+        } else {
+            LOGGER.warn("BlueMap не найден, маркеры недоступны");
+        }
 
-        CommandRegistrar.register();
-        ProtectionHandler.register();
-        BlueMapIntegration.tryInit();
+        LOGGER.info("WarPoliticalEngine полностью запущен!");
+    }
 
-        LOGGER.info("WarPoliticalEngine успешно загружен!");
+    private void onServerStopping(MinecraftServer server) {
+        if (dataManager != null) dataManager.saveAll();
+        if (blueMapIntegration != null) blueMapIntegration.shutdown();
+        LOGGER.info("WarPoliticalEngine остановлен");
     }
 
     public static WarPoliticalMod getInstance() {
         return instance;
     }
 
+    public MinecraftServer getServer() {
+        return server;
+    }
+
     public DataManager getDataManager() {
         return dataManager;
     }
 
+    public ScenarioParser getScenarioParser() {
+        return scenarioParser;
+    }
+
     public WorldManager getWorldManager() {
         return worldManager;
+    }
+
+    public ClaimEngine getClaimEngine() {
+        return claimEngine;
+    }
+
+    public BlueMapIntegration getBlueMapIntegration() {
+        return blueMapIntegration;
+    }
+
+    public Path getConfigDir() {
+        return FabricLoader.getInstance()
+                .getConfigDir().resolve(MOD_ID);
     }
 }
